6e0f9f45c1d930aefc415e14d16239734970eca2
Joel Trottier-Hebert <iamquebecois@gmail.com>
2017-09-13 14:24:12 -0400

    formatting

--- lib/models/life_entry.dart
+++ lib/models/life_entry.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'life_entry_activity.dart';
 
 class LifeEntry {
-  LifeEntry({this.startTime, this.endTime, this.lifeEntryActivities});
+  LifeEntry({
+    this.startTime,
+    this.endTime,
+    this.lifeEntryActivities,
+  });
 
   final TimeOfDay startTime;
   final TimeOfDay endTime;
--- lib/models/life_entry_activity.dart
+++ lib/models/life_entry_activity.dart
@@ -1,9 +1,13 @@
 class LifeEntryActivity {
-  LifeEntryActivity(
-      {this.activity, this.description, this.quantity, this.rating});
+  LifeEntryActivity({
+    this.activity,
+    this.description,
+    this.quantity,
+    this.rating,
+  });
 
-  String activity;
-  String description;
-  int quantity;
-  int rating;
+  final String activity;
+  final String description;
+  final int quantity;
+  final int rating;
 }
